b2daae2417f195eaef91b3414c4e7f6faeedc140
0dm <ashah775@outlook.com>
2021-01-16 16:11:10 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -21,12 +21,10 @@ def click(path):
         driver.quit()
 
 #The path to the location where the chromedriver is located at
-PATH = "chromedriver.exe"
+PATH = "chromedriver"
 
 #Automatically adds the profile 1 user to chrome to prevent any future sign ins after the first time
 options = webdriver.ChromeOptions()
-#options.add_argument('--user-data-dir=C:\\Users\\logan\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1')
-#options.add_argument('--profile-directory=Profile 1')
 
 driver = webdriver.Chrome(executable_path=PATH, options=options)
 
@@ -60,7 +58,6 @@ while not exit:
 # Sign in
 click("/html/body/div/form[1]/div/div/div[2]/div/div/div[1]/div[2]/div[2]/div/div[2]/div/div[3]/div[2]/div/div/div/div/input")
 
-
 #The while loop loops through the first page of classes (the active classes) and clicks on the desired class
 i = 0
 end = False
@@ -77,6 +74,8 @@ while end != True:
         end = True
 
 click('''//*[@id="app-messages-header"]/ng-include/chat-header/div/div[3]/button[1]''')
+
+#automatically turns the camera off if it is on
 try:
     time.sleep(3)
     element = WebDriverWait(driver, 5).until(
@@ -88,7 +87,7 @@ try:
 except:
     driver.quit()
 
-
+#automatically turns the microphone off if it is on
 try:
     time.sleep(3)
     element = WebDriverWait(driver, 5).until(
@@ -100,15 +99,17 @@ try:
 except:
     driver.quit()
 
+#Clicks the join button
 click('''//*[@id="page-content-wrapper"]/div[1]/div/calling-pre-join-screen/div/div/div[2]/div[1]/div[2]/div/div/section/div[1]/div/div/button''')
 
+#At a certain time, clicks the leave button
+
+click('''//*[@id="hangup-button"]''')
 time.sleep(5)
+click('''//*[@id="page-content-wrapper"]/div[1]/div/div/div[2]/div/div/button''')
 driver.quit()
 
-#Find a way to loop through website's chat bar's Xpath
-#Use the xpath and return the title associated with it
-#If the title == the desired class's title's Xpath
-#Then you enter the class
+
 
 
 
