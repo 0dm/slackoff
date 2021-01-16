71497d275661a8979569724f95d216ba1827799e
Logan Yu <loganyu438@gmail.com>
2021-01-16 16:08:04 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,10 @@
-import time
 import selenium
-import configparser
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import time
 c = ('Test Meeting').lower()
 
 #The click function clicks the desired path
@@ -21,45 +19,20 @@ def click(path):
         driver.quit()
 
 #The path to the location where the chromedriver is located at
-PATH = "chromedriver.exe"
+PATH = "D:\🐧\Coding stuff\Python files\chromedriver.exe"
 
 #Automatically adds the profile 1 user to chrome to prevent any future sign ins after the first time
 options = webdriver.ChromeOptions()
-#options.add_argument('--user-data-dir=C:\\Users\\logan\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1')
-#options.add_argument('--profile-directory=Profile 1')
+options.add_argument('--user-data-dir=C:\\Users\\logan\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1')
+options.add_argument('--profile-directory=Profile 1')
 
 driver = webdriver.Chrome(executable_path=PATH, options=options)
 
-# Parse ini file
-config = configparser.ConfigParser()
-config.read("config.ini")
-
 #The driver obtains the path to the website (Microsoft Teams)
-driver.get(config["Login"]["MeetingLink"])
+driver.get("https://teams.microsoft.com/_#/conversations/19:meeting_ZTJhYmIzYjItNjA0NS00NWZlLWI0ODItMDMyYzZmMTU2OGI2@thread.v2?ctx=chat")
 
-#Wait for the website to load in
+#Wait for 10 seconds for the website to load in
 time.sleep(5)
-
-user_path = "/html/body/div/form[1]/div/div/div[2]/div/div/div[1]/div[2]/div[2]/div/div/div/div[2]/div[2]/div/input[1]"
-pass_path = "/html/body/div/form[1]/div/div/div[2]/div/div/div[1]/div[2]/div[2]/div/div[2]/div/div[2]/div/div[2]/input"
-
-click(user_path)
-driver.find_element_by_xpath(user_path).send_keys(config["Login"]["Name"])
-click("/html/body/div/form[1]/div/div/div[2]/div/div/div[1]/div[2]/div[2]/div/div/div/div[4]/div/div/div/div/input")
-
-# go next
-exit = False
-while not exit:
-  try:
-    click(pass_path)
-    driver.find_element_by_xpath(pass_path).send_keys(config["Login"]["Password"])
-    exit = True
-  except:
-    pass
-
-# Sign in
-click("/html/body/div/form[1]/div/div/div[2]/div/div/div[1]/div[2]/div[2]/div/div[2]/div/div[3]/div[2]/div/div/div/div/input")
-
 
 #The while loop loops through the first page of classes (the active classes) and clicks on the desired class
 i = 0
@@ -77,6 +50,8 @@ while end != True:
         end = True
 
 click('''//*[@id="app-messages-header"]/ng-include/chat-header/div/div[3]/button[1]''')
+
+#automatically turns the camera off if it is on
 try:
     time.sleep(3)
     element = WebDriverWait(driver, 5).until(
@@ -88,7 +63,7 @@ try:
 except:
     driver.quit()
 
-
+#automatically turns the microphone off if it is on
 try:
     time.sleep(3)
     element = WebDriverWait(driver, 5).until(
@@ -100,15 +75,17 @@ try:
 except:
     driver.quit()
 
+#Clicks the join button
 click('''//*[@id="page-content-wrapper"]/div[1]/div/calling-pre-join-screen/div/div/div[2]/div[1]/div[2]/div/div/section/div[1]/div/div/button''')
 
+#At a certain time, clicks the leave button
+
+click('''//*[@id="hangup-button"]''')
 time.sleep(5)
+click('''//*[@id="page-content-wrapper"]/div[1]/div/div/div[2]/div/div/button''')
 driver.quit()
 
-#Find a way to loop through website's chat bar's Xpath
-#Use the xpath and return the title associated with it
-#If the title == the desired class's title's Xpath
-#Then you enter the class
+
 
 
 
