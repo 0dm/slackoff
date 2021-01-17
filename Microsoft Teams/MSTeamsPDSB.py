1364109973b531b0a10729cda163a7fcce163281
kevinchen220 <kc.chen.kevin@gmail.com>
2021-01-16 21:17:35 -0500

    msteams pdsb changes

--- Microsoft Teams/MSTeamsPDSB.py
+++ Microsoft Teams/MSTeamsPDSB.py
@@ -1,5 +1,5 @@
 import time
-
+import keyboard
 import selenium
 import configparser
 from selenium import webdriver
@@ -7,8 +7,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-c = ('Test Meeting').lower()
 
 
 # The click function clicks the desired path
@@ -28,10 +26,10 @@ def click(path):
 # The path to the location where the chromedriver is located at
 PATH = "chromedriver.exe"
 
+
 # Automatically adds the profile 1 user to chrome to prevent any future sign ins after the first time
 options = webdriver.ChromeOptions()
-options.add_argument("--disable-default-apps")
-options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized")
 options.add_experimental_option("prefs", {
     "profile.default_content_setting_values.media_stream_mic": 1,
     "profile.default_content_setting_values.media_stream_camera": 1,
@@ -42,46 +40,28 @@ driver = webdriver.Chrome(executable_path=PATH, options=options)
 # Parse ini file
 config = configparser.ConfigParser()
 config.read("config.ini")
-
+driver.get('https://teams.microsoft.com/_#/conversations/19:meeting_Nzc3NWFjOTctNTQxMC00NWI0LTk0YWMtZmE1Y2VlMzVlYjhm@thread.v2?ctx=chat')
 # The driver obtains the path to the website (Microsoft Teams)
-driver.get(config["Login"]["MeetingLink"])
+# Wait for the website to load in
 
-
-user_path = "/html/body/div/form[1]/div/div/div[2]/div/div/div[1]/div[2]/div[2]/div/div/div/div[2]/div[2]/div/input[1]"
-pass_path = "/html/body/div/form[1]/div/div/div[2]/div/div/div[1]/div[2]/div[2]/div/div[2]/div/div[2]/div/div[2]/input"
+user_path = '//*[@id="i0116"]'
+pass_path = '//*[@id="i0118"]'
 click(user_path)
 
 #Automatically inpust the username and proceeds next
 driver.find_element_by_xpath(user_path).send_keys(config["Login"]["Name"])
 click("/html/body/div/form[1]/div/div/div[2]/div/div/div[1]/div[2]/div[2]/div/div/div/div[4]/div/div/div/div/input")
+
 click('//*[@id="aadTile"]')
-click('//*[@id="i0118"]')
-
-#Automatically inpust the password and proceeds next
-driver.find_element_by_xpath('//*[@id="i0118"]').send_keys(config["Login"]["Password"])
-
-#Clicks next for the following pop-ups
+time.sleep(2)
+click(pass_path)
+driver.find_element_by_xpath(pass_path).send_keys(config["Login"]["Password"])
 click('//*[@id="idSIButton9"]')
 click('//*[@id="idSIButton9"]')
 
-#To make sure that the website is on the chat page
-click('//*[@id="chatstab"]/div/div/chat-list-bridge/div/div[1]/div/ul/li/div[2]/div[1]/a')
-
-# The while loop loops through the first page of classes (the active classes) and clicks on the desired class
-i = 0
-end = False
-while end != True:
-    i += 1
-    element = driver.find_element_by_xpath(
-        '''//*[@id="chatstab"]/div/div/chat-list-bridge/div/div[1]/div/ul/li/div[2]/div[%i]/a''' % i)
-    if i % 29 == 0:
-        click('''//*[@id="chatstab"]/div/div/chat-list-bridge/div/div[1]/div/ul/li/div[2]/div[%s]/a''' % (i))
-    if element.get_attribute('title').lower() == c:
-        click('''//*[@id="chatstab"]/div/div/chat-list-bridge/div/div[1]/div/ul/li/div[2]/div[%s]/a''' % (i))
-        end = True
-
-#Turns off the camera and microphone
-click('''//*[@id="app-messages-header"]/ng-include/chat-header/div/div[3]/button[1]''')
+driver.get((config["Login"]["MeetingLink"]))
+keyboard.send('enter')
+click('//*[@id="buttonsbox"]/button[2]')
 click(
     '//*[@id="page-content-wrapper"]/div[1]/div/calling-pre-join-screen/div/div/div[2]/div[1]/div[2]/div/div/section/div[2]/toggle-button[1]')
 click('//*[@id="preJoinAudioButton"]')
@@ -102,4 +82,3 @@ while not end2:
     if int(p) < 2:
         end2 = True
 click('''//*[@id="hangup-button"]''')
-
